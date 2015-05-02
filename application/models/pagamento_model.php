bddc5621a49c52e99ec4dd006b8638885cd0cc60
renan_rheinboldt <renan.lobato.rheinboldt@gmail.com>
2015-05-02 12:33:35 -0300

    Bd atualizado, alert de pagamento alterado

--- application/models/pagamento_model.php
+++ application/models/pagamento_model.php
@@ -5,10 +5,10 @@ class Pagamento_Model extends CI_Model
     public  function  criarComprovante($id,$dados){
         $this->db->where('idusuario', $id);
         if($this->db->update('usuario', $dados)){
-            $this->session->set_userdata('mensagem', 'Congratualations, wait confirm');
+            $this->session->set_userdata('Alert', 'Thank you. Wait for payment confirmation.');
         }
         else{
-            $this->session->set_userdata('mensagem', 'Something went wrong, please try again');
+            $this->session->set_userdata('Alert', 'Something went wrong, please try again');
         }
 
     }
