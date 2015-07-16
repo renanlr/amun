4e7e9a4efc4404719061491de9dadf4581bba790
Renan L Rheinboldt <renan.lobato.rheinboldt@gmail.com>
2015-07-16 00:35:01 -0300

    inclusão script Comite_has_paises

---
+++ others/insert_comite_has_paises.sql
@@ -0,0 +1,274 @@
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (13,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (14,	1, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (24,	1, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (29,	1, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (38,	1, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (170,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (172,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (173,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (175,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (176,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (177,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (131,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (178,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (178,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (133,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (135,	1, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (179,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (180,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (142,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (144,	1, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (145,	1, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (182,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (183,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (185,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (154,	5, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (156,	1, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (186,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (187,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (188,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (164,	1, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (189,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (7,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (7,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (2,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (2,	5, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (3,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (4,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (4,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (11,	1, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (12,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (12,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (12,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (15,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (16,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (16,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (16,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (16,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (17,	1, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (18,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (19,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (19,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (19,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (20,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (21,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (21,	5, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (22,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (22,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (22,	5, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (22,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (23,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (23,	5, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (23,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (25,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (25,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (25,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (25,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (26,	1, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (27,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (27,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (192,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (28,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (28,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (30,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (30,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (31,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (32,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (32,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (33,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (34,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (34,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (34,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (34,	5, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (34,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (35,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (35,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (35,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (35,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (36,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (78,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (78,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (39,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (40,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (40,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (40,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (41,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (42,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (42,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (42,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (42,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (43,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (43,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (44,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (44,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (44,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (45,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (46,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (46,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (46,	5, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (47,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (47,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (47,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (191,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (130,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (130,	5, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (132,	2, 5);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (134,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (134,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (136,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (136,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (137,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (137,	5, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (138,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (138,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (139,	5, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (140,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (141,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (141,	5, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (193,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (193,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (143,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (143,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (143,	5, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (146,	1, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (147,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (147,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (148,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (148,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (148,	5, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (148,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (149,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (150,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (150,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (151,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (152,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (152,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (152,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (153,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (153,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (155,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (157,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (157,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (157,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (158,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (158,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (159,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (159,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (160,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (160,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (161,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (162,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (162,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (162,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (162,	5, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (162,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (163,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (163,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (163,	4, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (163,	5, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (163,	6, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (165,	3, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (165,	5, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (166,	1, 2);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (167,	2, 1);
+INSERT INTO Comite_has_paises(paises_idpaises, Comite_idComite, qtd_vagas) VALUES (168,	3, 1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
